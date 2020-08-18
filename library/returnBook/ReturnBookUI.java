2dd5433034c92735ebc26a8cd2ee9f160cc545bc
navjeevan24 <68546405+navjeevan24@users.noreply.github.com>
2020-08-18 12:30:45 +1000

    Update ReturnBookUI.java
    Hey Dipanshu Kumar, I did some changes in code please request to my reviewer (Ankush Kumar) to check my code.

--- library/returnBook/ReturnBookUI.java
+++ library/returnBook/ReturnBookUI.java
@@ -49,12 +49,15 @@ public class ReturnBookUI {
 			case READY:
 				//String BoOk_InPuT_StRiNg = iNpUt("Scan Book (<enter> completes): ");
 				String BookInputString = input("Scan book (<enter> completes): ");
-				if (BoOk_InPuT_StRiNg.length() == 0) 
-					CoNtRoL.sCaNnInG_cOmPlEtE();
+				//if (BoOk_InPuT_StRiNg.length() == 0) 
+					if (Book_Input_String.Length() ==0)
+					//CoNtRoL.sCaNnInG_cOmPlEtE();
+					Control. Scanning_Complete();
 				
 				else {
 					try {
 						int Book_Id = Integer.valueOf(BoOk_InPuT_StRiNg).intValue();
+						 
 						CoNtRoL.bOoK_sCaNnEd(Book_Id);
 					}
 					catch (NumberFormatException e) {
