ef564f50a3172ad5df4a86858e8c1783bed4e4f5
dipanshu42 <68545877+dipanshu42@users.noreply.github.com>
2020-08-23 01:08:00 +1000

    Update pAY_fINE_cONTROL.java

--- library/payfine/pAY_fINE_cONTROL.java
+++ library/payfine/pAY_fINE_cONTROL.java
@@ -23,8 +23,8 @@ public class PayFineControl {                          // pAY_fINE_cONTROL
 			throw new RuntimeException("PayFineControl: cannot call setUI except in INITIALISED state");
 		}	
 		this.Ui = Ui;                //uI
-		uI.SeT_StAtE(PayFineUI.uI_sTaTe.READY);
-		StAtE = cOnTrOl_sTaTe.READY;		
+		Ui.setState(PayFineUI.UiState.READY);     //uI.SeT_StAtE(PayFineUI.uI_sTaTe.READY)
+		state= ControlState.READY;	           //StAtE = cOnTrOl_sTaTe.READY	
 	}
 
 
